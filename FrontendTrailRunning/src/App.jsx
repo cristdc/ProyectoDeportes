5c260e6fc58834d15ebc464cd048ad6b64ce4201
armandomarelius <armandomarelius777@gmail.com>
2025-03-04 11:57:59 +0100

    Contexto authContext hecho v1

--- FrontendTrailRunning/src/App.jsx
+++ FrontendTrailRunning/src/App.jsx
@@ -1,10 +1,14 @@
 import { RouterProvider } from "react-router-dom"
 import { router } from "./routes/router"
+import { AuthProvider } from "./context/AuthContext"
+
 
 const App = () => {
   return (
     <>
-      <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
     </>
   )
 }
