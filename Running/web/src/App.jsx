d26c8be59d8d68fab4cb0bb05768ec588637c8e5
Destructordead32 <pedrojaviermarquelizana@gmail.com>
2025-03-04 10:52:11 +0100

    primer commit inicial app debugear

--- Running/web/src/App.jsx
+++ Running/web/src/App.jsx
@@ -1,13 +1,9 @@
-import React from 'react'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './router'
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes.jsx'; 
+import './styles/index.css';
 
-const App = () => {
-  return (
-    <div>
-      <RouterProvider router={router} />
-    </div>
-  )
+function App() {
+  return <RouterProvider router={router} />;
 }
 
 export default App;
