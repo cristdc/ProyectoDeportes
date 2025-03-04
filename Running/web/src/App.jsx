6eb094bbfbbad4d1d658d4343851e8ce4b922a2f
Destructordead32 <pedrojaviermarquelizana@gmail.com>
2025-03-04 10:52:11 +0100

    primer commit inicial app debugear

--- Running/web/src/App.jsx
+++ Running/web/src/App.jsx
@@ -1,6 +1,6 @@
 import { RouterProvider } from 'react-router-dom';
-import { router } from './router/index.jsx';
-import './index.css';
+import { router } from './routes.jsx'; // Añadimos la extensión .jsx
+import './styles/index.css';
 
 function App() {
   return <RouterProvider router={router} />;
--- Running/web/src/router/index.jsx
+++ Running/web/src/router/index.jsx
@@ -1,18 +1,13 @@
 import { createBrowserRouter } from 'react-router-dom';
-import RootLayout from '../layout/RootLayout.jsx';
-import Home from '../pages/Home.jsx';
-import AvailableRaces from '../pages/AvailableRaces.jsx';
-import Participate from '../pages/Participate.jsx';
-import LoginPage from '../pages/LoginPage.jsx';
-import ProfilePage from '../pages/ProfilePage.jsx';
-import HistoryPage from '../pages/HistoryPage.jsx';
-import ErrorPage from '../pages/ErrorPage.jsx';
+import RootLayout from './layouts/RootLayout.jsx';
+import Home from './pages/Home/Home.jsx';
+import AvailableRaces from './pages/AvailableRaces/AvailableRaces.jsx';
+import Participate from './pages/Participate/Participate.jsx';
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout />,
-    errorElement:<ErrorPage/>,
     children: [
       {
         index: true,
@@ -35,7 +30,7 @@ export const router = createBrowserRouter([
         element: <AvailableRaces />
       },
       {
-        path: 'historial',
+        path: 'participar',
         element: <Participate />
       }
     ]
