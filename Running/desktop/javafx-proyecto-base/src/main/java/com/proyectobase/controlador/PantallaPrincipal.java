fc0e7b46621764f97994f7d1c4fd7cb7235cfc48
cristdc <cristinadiazcabello@gmail.com>
2025-03-05 12:09:42 +0100

    Actualizada Pantalla Principal

--- Running/desktop/javafx-proyecto-base/src/main/java/com/proyectobase/controlador/PantallaPrincipal.java
+++ Running/desktop/javafx-proyecto-base/src/main/java/com/proyectobase/controlador/PantallaPrincipal.java
@@ -1,11 +1,20 @@
+package com.proyectobase.controlador;
+
 import java.awt.event.MouseEvent;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.image.ImageView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class PantallaPrincipal implements Initializable {
 
@@ -17,9 +26,18 @@ public class PantallaPrincipal implements Initializable {
 
     @FXML
     private TableView<?> tableViewPuestos;
+    
+    @FXML
+    private TableColumn<?,?> columnNombre;
+    
+    @FXML
+    private TableColumn<?,?> columnPuesto;
+    
+    @FXML
+    private TableColumn<?,?> columnTiempo;
 
     @FXML
-    private Label txt;
+    private Label txtParticipar;
 
     @FXML
     private Label txtCarreras;
@@ -33,10 +51,6 @@ public class PantallaPrincipal implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         
-    }
-    @FXML
-    void navegarVentanaCarreras(MouseEvent event) {
-
     }
 
 }
