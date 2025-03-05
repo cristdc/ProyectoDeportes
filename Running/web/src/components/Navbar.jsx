83610245b71b9a02a17cbf8c3b112c3257f80518
Kuisyy <luis3valderrubio@gmail.com>
2025-03-05 11:54:39 +0100

    Home arreglado

--- Running/web/src/components/Navbar.jsx
+++ Running/web/src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import { FaHistory, FaUser } from 'react-icons/fa';
 import { MdEmojiEvents } from 'react-icons/md';
 import { NavLink, Link } from 'react-router-dom';
-import { useAuth } from '../context/AuthContext'
+import { useAuth } from '../context/AuthContext';
 
 const Navbar = () => {
-  const { user }=useAuth();
+    const { user } =useAuth()
+
   return (
     <nav className="nav-container">
       <div className="nav-content">
@@ -28,24 +29,23 @@ const Navbar = () => {
               <span className="nav-link-icon"><FaHistory /></span>
               <span>Historial</span>
             </NavLink>
-
-            {user ?
-            (   <NavLink 
-              to="/profile" 
-              className={({ isActive }) => `nav-link ${isActive ? 'nav-link-active' : ''}`}
-            >
-              <span className="nav-link-icon"><FaUser /></span>
-              <span>Perfil</span>
-            </NavLink>)
-            
-            :(   <NavLink 
-              to="/login" 
-              className={({ isActive }) => `nav-link ${isActive ? 'nav-link-active' : ''}`}
-            >
-              <span className="nav-link-icon"><FaUser /></span>
-              <span>Login</span>
-            </NavLink>)}
-         
+         {user ? 
+         (   <NavLink 
+          to="/profile" 
+          className={({ isActive }) => `nav-link ${isActive ? 'nav-link-active' : ''}`}
+        >
+          <span className="nav-link-icon"><FaUser /></span>
+          <span>Perfil</span>
+        </NavLink>)
+         :(
+          <NavLink 
+          to="/login" 
+          className={({ isActive }) => `nav-link ${isActive ? 'nav-link-active' : ''}`}
+        >
+          <span className="nav-link-icon"><FaUser /></span>
+          <span>Login</span>
+        </NavLink>
+         )}
           </div>
         </div>
       </div>
