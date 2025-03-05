221d7fd5cb64f54a8d325a1a68724a01aac4fbc9
javiilpf <javieraviless2003@gmail.com>
2025-03-05 10:24:41 +0100

    Creación de la página de perfil y solución de error en app.jsx

--- Running/web/src/context/AuthContext.jsx
+++ Running/web/src/context/AuthContext.jsx
@@ -62,6 +62,7 @@ export const AuthProvider = ({ children }) => {
         loading,
         login,
         logout,
+        
     };
 
     return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
