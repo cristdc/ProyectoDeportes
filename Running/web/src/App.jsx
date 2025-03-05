f72c4e8adb5d2d6cef94d64f2feac4b9a9688562
Jhonsking49 <juanreygonzalez2002@gmail.com>
2025-03-05 10:10:31 +0100

    Login terminado

--- Running/web/src/App.jsx
+++ Running/web/src/App.jsx
@@ -1,9 +1,19 @@
 import { RouterProvider } from 'react-router-dom';
 import { router } from './router/index.jsx';
 import './index.css';
+import { AuthProvider } from './context/AuthContext.jsx';
 
-function App() {
-  return <RouterProvider router={router} />;
+const App = () => {
+  return (
+    <>
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+      
+    </>
+    
+  )
 }
+
 
 export default App;
--- Running/web/src/pages/LoginPage.jsx
+++ Running/web/src/pages/LoginPage.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import { useAuth } from '../context/AuthContext';
 import { toast } from 'sonner';
+import "../styles/main.css";
 
 const LoginPage = () => {
     const [loading, setLoading] = useState(false);
@@ -29,9 +30,47 @@ const LoginPage = () => {
         }
     };
 
-  return (
-    <div>LoginPage</div>
-  )
-}
+    return (
+        <div className="login-container">
+            <div className="login-box">
+                <h2 className="login-title">Iniciar Sesión</h2>
+                
+                <form onSubmit={handleSubmit} className="login-form">
+                    <div>
+                        <label htmlFor="email" className="text-sm">Email</label>
+                        <input
+                            id="email"
+                            name="email"
+                            type="email"
+                            placeholder='tu@email.com'
+                            required
+                            className="input-field"
+                        />
+                    </div>
+                    
+                    <div>
+                        <label htmlFor="password" className="text-sm">Contraseña</label>
+                        <input
+                            id="password"
+                            name="password"
+                            type="password"
+                            placeholder='*******'
+                            required
+                            className="input-field"
+                        />
+                    </div>
 
-export default LoginPage
+                    <button
+                        type="submit"
+                        disabled={loading}
+                        className="login-button"
+                    >
+                        {loading ? 'Iniciando sesión...' : 'Iniciar Sesión'}
+                    </button>
+                </form>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;
