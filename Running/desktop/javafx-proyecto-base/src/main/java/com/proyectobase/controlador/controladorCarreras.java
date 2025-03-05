c7fa99845904c13aaf19f7016c496b30904abefb
santigm12 <159147660+santigm12@users.noreply.github.com>
2025-03-05 10:28:29 +0100

    Ventana y Controlador Login y Carreras

---
+++ Running/desktop/javafx-proyecto-base/src/main/java/com/proyectobase/controlador/controladorCarreras.java
@@ -0,0 +1,37 @@
+package com.proyectobase.controlador;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+/**
+ *
+ * @author Santiago
+ */
+
+public class controladorCarreras implements Initializable{
+    
+    @FXML
+    private Button btnIniciarSesion;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtUsuario;
+
+    @FXML
+    void btnAccionIniciarSesion(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }
+}
