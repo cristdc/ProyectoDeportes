6b9bc4edbb4bdde39b87a1259ca751a7db2914d6
Juannxnn <juuanxn04@gmail.com>
2025-03-05 12:30:30 +0100

    Ventana login

---
+++ Front/javafx-proyecto-base/src/main/java/com/Controladores/ControladorLogin.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Controladores;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Juann
+ */
+public class ControladorLogin implements Initializable{
+    
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private PasswordField txtContraseña;
+
+    @FXML
+    private TextField txtCorreo;
+
+    @FXML
+    void login(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    
+}
