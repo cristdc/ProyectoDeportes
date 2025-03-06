376e611989a1f8e3ff979391a80070b1a81f8ea3
jesusjipozo <jesusjipozo@gmail.com>
2025-03-06 08:35:20 +0100

    cambios actualizados8

--- Running/desktop/javafx-proyecto-base/src/main/java/com/proyectobase/controlador/controladorCarreras.java
+++ Running/desktop/javafx-proyecto-base/src/main/java/com/proyectobase/controlador/controladorCarreras.java
@@ -24,7 +24,7 @@ public class controladorCarreras implements Initializable{
     private Button btnDesapuntarse;
 
     @FXML 
-    private Button btnPar ticipar;
+    private Button btnPar;
 
     @FXML
     private TableColumn<Carrera, Double> columnDistancia;
