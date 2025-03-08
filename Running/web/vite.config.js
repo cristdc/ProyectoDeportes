cfb1791e36df87d5ed67fa8fc18919eabc831740
Ana <ana24garcia07@gmail.com>
2025-03-08 13:29:34 +0100

    Changes in nginx Running

--- Running/web/vite.config.js
+++ Running/web/vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-   build: {
-    outDir: "dist", 
-    assetsDir: "", 
+  build: {
+    outDir: "dist",
+    assetsDir: "assets", 
   },
-  base: "/running",
+  base: "/running/", 
   plugins: [react(), tailwindcss()],
   resolve: {
     extensions: [".js", ".jsx"],
