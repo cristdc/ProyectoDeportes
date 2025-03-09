e187e713ba8064b1c8f92048d7bc040b6898c96e
Ana <ana24garcia07@gmail.com>
2025-03-09 18:33:32 +0100

    Update vite.config.js Cycling

--- Cycling/web/vite.config.js
+++ Cycling/web/vite.config.js
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: "dist",
+    assetsDir: "assets", 
+  },
+  base: "/cycling/", 
   plugins: [react(), tailwindcss()],
-})
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+});
